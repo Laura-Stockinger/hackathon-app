614749aa081f03e3730df8f696fd0ace84382388
caralk99 <caralk99@gmail.com>
2024-06-22 10:31:51 +0000

    Fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:amplify_api/amplify_api.dart';
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +15,6 @@ Future<void> _configureAmplify() async {
   try {
     await Amplify.addPlugins(
       [
-        AmplifyAuthCognito(),
         AmplifyAPI(
           options: APIPluginOptions(
             modelProvider: ModelProvider.instance,
